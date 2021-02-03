1bee4fa2a9f45e52969611ee88a7438d921c7172
yarrabotula <39594771+yarrabotula@users.noreply.github.com>
2021-02-03 15:54:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is Love</h1>");
+  response.end("<h1> Deployed Version is V1.2.3</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
